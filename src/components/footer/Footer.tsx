b9c062f277855dbda7a222405b7e40f17276975e
dipeshdura <dipeshdura14@gmail.com>
2024-06-20 02:03:46 +0545

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,10 @@ import './footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <span>DurazAdmin</span>
+      <span>©️ Duraz Admin Dashboard</span>
+    </div>
   )
 }
 
